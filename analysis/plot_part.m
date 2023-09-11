af9226328b8afd52f0ab0f0af795e6aabf4b9409
Zhao-Han Zhang <139232905+zzh535897@users.noreply.github.com>
2023-09-11 17:48:20 +0800

    Update plot_part.m

--- analysis/plot_part.m
+++ analysis/plot_part.m
@@ -3,7 +3,7 @@ if(exist('lmd1','var'))
     figure;hold on;
     for im=1:nm
         for il=1:nl
-            plot(pr.^2/2,squeeze(abs(lmd(im,il,:))).^2,'LineWidth',1.1);
+            plot(pr.^2/2,pr.*squeeze(abs(lmd(im,il,:))).^2,'LineWidth',1.1);
         end
     end
     xlabel('$E$(a.u.)'        ,'Interpreter','latex');
@@ -11,7 +11,7 @@ if(exist('lmd1','var'))
     title('Partial-Wave PMD');
 
     figure;hold on;
-    pes=sum(sum(abs(lmd).^2,1),2);
+    pes=pr.*sum(sum(abs(lmd).^2,1),2);
     plot(pr.^2/2,pes,'k-','LineWidth',1.1);
 
     xlabel('$E$(a.u.)'   ,'Interpreter','latex');
